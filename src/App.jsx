f82a7146bf1b793f55197f3d6a7525cbda7cff54
Evelyn Salomão <evelinesalomao775@gmail.com>
2026-03-19 20:34:36 -0300

    feat: adiciona footer responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      <h1 className="text-white text-4xl">
-        Projeto funcionando 🚀
-      </h1>
+    <div className="min-h-screen flex flex-col bg-black w-full overflow-x-hidden">
+      
+      {/* O conteúdo principal do site */}
+      <main className="flex-grow flex items-center justify-center w-full">
+        <h1 className="text-white text-4xl font-bold">Projeto funcionando 🚀</h1>
+      </main>
+
+      {/* O Footer agora está livre para ocupar a largura total */}
+      <Footer />
+      
     </div>
   )
 }
