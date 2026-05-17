2b17b390e25972f3231ca84a942eec849c30bf5d
Hilan <hilan.paganini@gmail.com>
2026-05-17 14:59:30 -0300

    feat: add ong image upload

--- server/src/services/ong-service.ts
+++ server/src/services/ong-service.ts
@@ -5,6 +5,7 @@ import {
 	ForbiddenError,
 } from "@/types/custom-errors";
 import type { OngQueryParams, OngRequest } from "@/types/models/ong-types";
+import { imageService } from "./image-service";
 
 export const ongService = {
 	getOngById: async (id: string) => {
@@ -12,13 +13,21 @@ export const ongService = {
 		if (result.length === 0) {
 			throw new EntityNotFound("ONG não encontrada");
 		}
+		result[0].urlImagem = imageService.buildPublicUrl(result[0].urlImagem);
 		return result[0];
 	},
 	createOng: async (request: OngRequest, userId: string) => {
+		const urlImagem = await imageService.uploadImage(request.imagem, "ongs");
 		try {
-			const result = await ongRepository.createOng(request, userId);
+			const result = await ongRepository.createOng({
+				...request,
+				urlImagem,
+				userId,
+			});
+			result[0].urlImagem = imageService.buildPublicUrl(result[0].urlImagem);
 			return result[0];
 		} catch {
+			await imageService.deleteImage(urlImagem);
 			throw new DatabaseError(
 				"Erro inesperado ao criar ONG. CNPJ, email ou usuário podem já estar cadastrados em outra ONG.",
 			);
@@ -29,30 +38,47 @@ export const ongService = {
 		userId: string,
 		request: Partial<OngRequest>,
 	) => {
+		// primeiro realiza todas as validações
+		const ongResult = await ongRepository.getOngById(ongId);
+		if (ongResult.length === 0) {
+			throw new EntityNotFound("ONG não encontrada");
+		}
+		if (ongResult[0].userId !== userId) {
+			throw new ForbiddenError("ONG informada não pertence ao usuário atual");
+		}
+		// salva a nova imagem se existir no request
+		const antigaImagem = ongResult[0].urlImagem;
+		let novaImagem: string | undefined;
+		if (request.imagem) {
+			novaImagem = await imageService.uploadImage(request.imagem, "ongs");
+		}
+		// salva os dados no banco e apaga a nova imagem em caso de erro
 		let updateResult: Awaited<ReturnType<typeof ongRepository.updateOng>>;
 		try {
-			updateResult = await ongRepository.updateOng(ongId, userId, request);
+			updateResult = await ongRepository.updateOng(ongId, userId, {
+				...request,
+				urlImagem: novaImagem,
+			});
 		} catch {
+			if (novaImagem) {
+				await imageService.deleteImage(novaImagem);
+			}
 			throw new DatabaseError(
 				"Erro inesperado ao atualizar ONG. CNPJ ou email podem já estar cadastrados em outra ONG.",
 			);
 		}
-		if (updateResult.length === 0) {
-			const userOngResult = await ongRepository.getOngAndUserIds(userId);
-			if (userOngResult.length === 0) {
-				throw new EntityNotFound("Usuário atual não possui ONG cadastrada");
-			}
-			const ongExists = await ongRepository.ongExistsById(ongId);
-			if (!ongExists) {
-				throw new EntityNotFound("ONG não encontrada");
-			}
-			if (userOngResult[0].ongId !== ongId) {
-				throw new ForbiddenError("ONG informada não pertence ao usuário atual");
-			}
+		// após dar tudo certo, apaga a imagem antiga caso exista uma nova imagem
+		if (novaImagem) {
+			await imageService.deleteImage(antigaImagem);
 		}
+		updateResult[0].urlImagem = imageService.buildPublicUrl(
+			updateResult[0].urlImagem,
+		);
 		return updateResult[0];
 	},
 	getOngs: async (params: OngQueryParams) => {
-		return ongRepository.getOngs(params);
+		const result = await ongRepository.getOngs(params);
+		result.forEach((ong) => ong.urlImagem = imageService.buildPublicUrl(ong.urlImagem));
+		return result;
 	},
 };
--- server/src/types/models/ong-types.ts
+++ server/src/types/models/ong-types.ts
@@ -1,6 +1,15 @@
+import type { InferInsertModel } from "drizzle-orm";
 import type { UnwrapSchema } from "elysia";
-import type { OngBodyParse, OngQueryParamsParse } from "@/types/schemas/ong-schemas";
+import type { ong } from "@/database/schema";
+import type {
+	OngBodyParse,
+	OngQueryParamsParse,
+} from "@/types/schemas/ong-schemas";
 
 export type OngRequest = UnwrapSchema<typeof OngBodyParse>;
 
 export type OngQueryParams = UnwrapSchema<typeof OngQueryParamsParse>;
+
+export type OngInsert = InferInsertModel<typeof ong>;
+
+export type OngUpdate = Partial<OngInsert>;
