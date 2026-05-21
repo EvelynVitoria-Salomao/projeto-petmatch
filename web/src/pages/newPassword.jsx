8ad126adba9c097dca71fc142bb2372610f3e8e8
Gustavo <144969535+Gustavo08Lima@users.noreply.github.com>
2026-05-21 10:57:35 -0300

    alterando a forma de carregamento do forms

--- web/src/pages/newPassword.jsx
+++ web/src/pages/newPassword.jsx
@@ -1,11 +1,25 @@
 import { Link } from 'react-router-dom'
 import LogoTipo from "../assets/icons/Logo.svg"
+import { useNavigate } from 'react-router-dom';
 
 function NewPassword() {
+
+
+    const navigate = useNavigate();
+
+        const handleSubmit = (e) => {
+            e.preventDefault(); // Evita o recarregamento da página
+            // Aqui depois você coloca a lógica de enviar para o back-end...
+            
+            navigate('/Login/checked'); // Redireciona via código
+        };
+
+
+
     return (
         <>
        
-            <form className="flex justify-center mt-10 md:mt-[10%] px-4 w-full">
+            <form onSubmit={handleSubmit} className="flex justify-center mt-10 md:mt-[10%] px-4 w-full">
                 
           
                 <div className="rounded-2xl border-3 border-[#27607B] w-full max-w-[507px] min-h-[454px] flex flex-col items-center justify-center p-6 md:p-8 bg-white box-border">
@@ -29,14 +43,14 @@ function NewPassword() {
                     />
                   
                    
-                    <Link className="w-full max-w-[420px] flex justify-center" to="/Login/checked">
+                   
                         <button 
-                            type="button"
+                            type="submit"
                             className="font-['Poppins'] border border-[#27607B] bg-[#E5B300] text-white rounded-2xl w-full h-[45px] mb-6 text-[20px] cursor-pointer hover:brightness-105 transition-all"
                         >
                             Enviar
                         </button>
-                    </Link>
+               
                     
                     <Link className="text-[#27607B] text-[18px] font-['Poppins'] cursor-pointer hover:underline" to="/Login">
                         Voltar
